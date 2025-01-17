7adfa898c7e099e092b69c89a0c8ec433ea60afe
Aastha <aasthasinghddun289@gmail.com>
2025-01-17 20:07:48 +0530

    fixed image bug

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    domains: ['api.dicebear.com'],
+    dangerouslyAllowSVG: true, // Allow SVG images
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", 
+
+  },
 };
 
 export default nextConfig;
